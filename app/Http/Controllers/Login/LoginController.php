4e9912f0cba80e15ef5ad2edb36a9609680f42b3
lankaize <2728701189@qq.com>
2020-07-17 16:00:59 +0800

    存入session

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Login;
 use App\Http\Controllers\Controller;
 use App\Model\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 class LoginController extends Controller
 {
     //登录
@@ -21,8 +22,10 @@ class LoginController extends Controller
         }
         if($where){
             session(['res'=>$where]);
+            echo"登录成功";die;
             return redirect("user/index");
         }else{
+            echo"登录失败";die;
             return redirect("login/login")->with("msg","用户或密码错误");
         }
 
